db887a7d8ae9aa6230417538149a21d0d98d6550
VicBVic <0.BotnaruVictor.0@gmail.com>
2021-12-03 15:21:06 +0200

    first rendition of the postare

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:clean_our_cities/postare.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,30 +41,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return Postare();
   }
 }
--- lib/postare.dart
+++ lib/postare.dart
@@ -10,6 +10,22 @@ class Postare extends StatefulWidget {
 class _PostareState extends State<Postare> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+
+      color: Colors.white,
+      child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Look at this"
+          ),
+          Image.network(
+              "https://clipart-best.com/img/trollface/trollface-clip-art-7.png",
+
+          ),
+        ],
+      ),
+    );
   }
 }
