bf51cf980b6ba760243c2c374b4c8d7c040d4651
VicBVic <0.BotnaruVictor.0@gmail.com>
2021-12-03 15:04:56 +0200

    messagE

---
+++ test/postare.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Postare extends StatefulWidget {
+  const Postare({Key? key}) : super(key: key);
+
+  @override
+  _PostareState createState() => _PostareState();
+}
+
+class _PostareState extends State<Postare> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
