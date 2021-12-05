ee92920271ed9d56aebfc8bab7f8fc60833ef06a
VicBVic <0.BotnaruVictor.0@gmail.com>
2021-12-05 10:32:25 +0200

    a couple stuff

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         textTheme: ThemeData.dark().textTheme,
       ),
       darkTheme: ThemeData(),
-      home: MainFeed(),
+      home:  ScaffoldMain(),
     );
   }
 }
--- lib/menus/main_feed/new_arrivals.dart
+++ lib/menus/main_feed/new_arrivals.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clean_our_cities/post/postare.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:clean_our_cities/menus/post_menu.dart';
 class HorizontalList extends StatelessWidget {
   List<DocumentSnapshot> posts=[];
   String headline="";
@@ -27,47 +27,58 @@ class HorizontalList extends StatelessWidget {
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemBuilder: (context,index){
-                  return Container(
-                    width: 250.0,
-                    height: 250.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Stack(
-
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(15.0),
-                          child: Image.network(
-                            posts[index].data["path"]==null? "https://esquilo.io/png/thumb/KoRirtV657T6PHB-Trollface-PNG-Image.png":posts[index].data["path"],
-                            //width:250.0,
-                            height: 250.0,
-                          fit: BoxFit.cover,
+                  return TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) =>  PostMenu(
+                              posts[index].data["path"],
+                            ),
                           ),
-                        ),
+                      );
+                    },
+                    child: Container(
+                      width: 250.0,
+                      height: 250.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Stack(
 
-                      Container(
-                         padding: const EdgeInsets.all(5.0),
-                      alignment: Alignment.bottomCenter,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                               colors: <Color>[
-                                Colors.black.withAlpha(0),
-                                Colors.black12,
-                                Colors.black45
-                                  ],
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Image.network(
+                              posts[index].data["path"]==null? "https://esquilo.io/png/thumb/KoRirtV657T6PHB-Trollface-PNG-Image.png":posts[index].data["path"],
+                              //width:250.0,
+                              height: 250.0,
+                            fit: BoxFit.cover,
                             ),
                           ),
 
-                        child: Text(
-                          posts[index].data["title"],
-                          style: Theme.of(context).textTheme.headline5,
+                        Container(
+                           padding: const EdgeInsets.all(5.0),
+                        alignment: Alignment.bottomCenter,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                 colors: <Color>[
+                                  Colors.black.withAlpha(0),
+                                  Colors.black12,
+                                  Colors.black45
+                                    ],
+                              ),
+                            ),
+
+                          child: Text(
+                            posts[index].data["title"],
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
                         ),
+                        ],
                       ),
-                      ],
                     ),
                   );
               },
